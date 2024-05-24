f57095b2f5fad9127fb6672f124a6ae485c25b1d
haixuantao <tao.xavier@outlook.com>
2024-05-24 11:13:16 +0200

    Adding Current Based Position Control to avoid Overloading Errors

--- aloha/nodes/aloha-teleop/src/main.rs
+++ aloha/nodes/aloha-teleop/src/main.rs
@@ -132,6 +132,13 @@ fn main() -> Result<()> {
             &[1; 9],
         )
         .expect("Communication error");
+        xm::sync_write_operating_mode(
+            &io,
+            puppet_right_serial_port.as_mut(),
+            &[1, 2, 3, 4, 5, 6, 7, 8, 9],
+            &[5; 9],
+        )
+        .expect("Communication error");
         Some(main_multithreaded(
             io,
             master_right_serial_port,
@@ -160,6 +167,16 @@ fn main() -> Result<()> {
         &[1; 9],
     )
     .expect("Communication error");
+
+    // Set operating mode to current based position control
+    xm::sync_write_operating_mode(
+        &io,
+        puppet_left_serial_port.as_mut(),
+        &[1, 2, 3, 4, 5, 6, 7, 8, 9],
+        &[5; 9],
+    )
+    .expect("Communication error");
+
     let join_left = main_multithreaded(
         io,
         master_left_serial_port,
