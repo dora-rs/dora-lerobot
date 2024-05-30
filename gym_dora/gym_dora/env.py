fd7f1646e4a1e65baa36b1890e100eca14931d33
haixuantao <tao.xavier@outlook.com>
2024-05-30 11:49:08 +0200

    Updating dora refresh logic accounting for FPS

--- gym_dora/gym_dora/env.py
+++ gym_dora/gym_dora/env.py
@@ -5,12 +5,19 @@ import numpy as np
 import pyarrow as pa
 from dora import Node
 from gymnasium import spaces
+import time
 
 
 class DoraEnv(gym.Env):
     metadata = {}
 
-    def __init__(self, fps: int, actions: list[str], joints: list[str] | None = None, cameras: dict[str, tuple[int]] | None = None):
+    def __init__(
+        self,
+        fps: int,
+        actions: list[str],
+        joints: list[str] | None = None,
+        cameras: dict[str, tuple[int]] | None = None,
+    ):
         self.fps = fps
         self.actions = actions
         self.joints = joints
@@ -39,45 +46,52 @@ class DoraEnv(gym.Env):
                     shape=hwc_shape,
                     dtype=np.uint8,
                 )
-            observation_space["pixels"] = pixels_space
+            observation_space["pixels"] = spaces.Dict(pixels_space)
 
         self.observation_space = spaces.Dict(observation_space)
-        self.action_space = spaces.Box(low=-1, high=1, shape=(len(self.actions),), dtype=np.float32)
+        self.action_space = spaces.Box(
+            low=-1, high=1, shape=(len(self.actions),), dtype=np.float32
+        )
 
         # Initialize a new Dora node used to get events from the robot
         # that will be stored in `_observation` and `_terminated`
         self._node = Node()
         self._observation = {"pixels": {}, "agent_pos": None}
         self._terminated = False
+        self._step_time = time.time()
 
     def _get_obs(self):
-        while True:
+        obs_initial_time = time.time()
+        while time.time() - obs_initial_time < 1/self.fps:
             event = self._node.next(timeout=0.001)
 
             ## If event is None, the node event stream is closed and we should terminate the env
             if event is None:
                 self._terminated = True
-                break
+                print("Node event stream closed.")
+                raise ConnectionError("Dora Node event stream closed.")
 
             if event["type"] == "INPUT":
                 # Map Image input into pixels key within Aloha environment
                 if "cam" in event["id"]:
-                    camera = event["id"]
-                    hwc_shape = self.cameras[camera]
-                    self._observation["pixels"][camera] = (
-                        event["value"].to_numpy().reshape(hwc_shape)
+                    self._observation["pixels"][event["id"]] = (
+                        event["value"]
+                        .to_numpy()
+                        .reshape(self.cameras[event["id"]])
+                        .copy()
                     )
                 else:
                     # Map other inputs into the observation dictionary using the event id as key
                     self._observation[event["id"]] = event["value"].to_numpy()
 
             # If the event is a timeout error break the update loop.
-            elif event["type"] == "ERROR":
+            if event["type"] == "ERROR":
                 break
 
     def reset(self, seed: int | None = None):
         del seed
-        self._node.send_output("reset")
+        ## TODO(tao): Add reset event to the node
+        # self._node.send_output("reset")
         self._get_obs()
         self._terminated = False
         info = {}
@@ -86,6 +100,12 @@ class DoraEnv(gym.Env):
     def step(self, action: np.ndarray):
         # Send the action to the dataflow as action key.
         self._node.send_output("action", pa.array(action))
+
+        # Space observation so that they match the dataset
+        time.sleep(max(0, time.time() - self._step_time - 1 / self.fps))
+        self._step_time = time.time()
+
+        # Reset the observation
         self._get_obs()
         reward = 0
         terminated = truncated = self._terminated
