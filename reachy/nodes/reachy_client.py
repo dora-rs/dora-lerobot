ed311c1283828fbb6c7ad40e524a51e635f6a991
apirrone <awanegenebayo@gmail.com>
2024-06-11 12:19:33 +0200

    working reachy2 monotask mug

--- reachy/nodes/reachy_client.py
+++ reachy/nodes/reachy_client.py
@@ -11,7 +11,7 @@ from reachy2_sdk import ReachySDK
 
 freq = 30
 
-ROBOT_IP = "192.168.1.51"
+ROBOT_IP = "192.168.1.42"
 # ROBOT_IP = "localhost"
 
 reachy = ReachySDK(ROBOT_IP)
@@ -22,42 +22,29 @@ reachy.turn_on()
 
 time.sleep(1)
 action = [
-    -0.11903145498601328,
-    0.11292280260403312,
-    0.48048914307403895,
-    -1.4491468779308918,
-    0.1895427567665842,
-    0.009599310885968814,
-    -0.20141099568014562,
-    2.2656896114349365,
-    -0.13212142437597074,
-    -0.07731808586334879,
-    -0.5141739976375295,
-    -1.512502329778286,
-    0.00034906585039886593,
-    0.3193952531149623,
-    0.40474185353748504,
-    2.2610876560211,
+    2.70750225e-02,
+    3.76733482e-01,
+    -1.79577127e-01,
+    -1.60979617e00,
+    -1.09046809e-02,
+    6.97152093e-02,
+    1.92173451e-01,
+    1.71317685e00,
+    5.88761508e-01,
+    -7.00486451e-03,
+    -1.59729898e-01,
+    -2.03063869e00,
+    -1.12172333e-04,
+    -3.93478051e-02,
+    1.20750383e-01,
+    1.96845388e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    4.07035090e-02,
+    4.09805328e-01,
+    -1.20783195e-01,
 ]
-
-
-# reachy.l_arm.shoulder.pitch.goal_position = np.rad2deg(action[0])
-# reachy.l_arm.shoulder.roll.goal_position = np.rad2deg(action[1])
-# reachy.l_arm.elbow.yaw.goal_position = np.rad2deg(action[2])
-# reachy.l_arm.elbow.pitch.goal_position = np.rad2deg(action[3])
-# reachy.l_arm.wrist.roll.goal_position = np.rad2deg(action[4])
-# reachy.l_arm.wrist.pitch.goal_position = np.rad2deg(action[5])
-# reachy.l_arm.wrist.yaw.goal_position = np.rad2deg(action[6])
-# reachy.l_arm.gripper.set_opening(min(100, max(0, action[7] * 40)))
-
-# reachy.r_arm.shoulder.pitch.goal_position = np.rad2deg(action[8])
-# reachy.r_arm.shoulder.roll.goal_position = np.rad2deg(action[9])
-# reachy.r_arm.elbow.yaw.goal_position = np.rad2deg(action[10])
-# reachy.r_arm.elbow.pitch.goal_position = np.rad2deg(action[11])
-# reachy.r_arm.wrist.roll.goal_position = np.rad2deg(action[12])
-# reachy.r_arm.wrist.pitch.goal_position = np.rad2deg(action[13])
-# reachy.r_arm.wrist.yaw.goal_position = np.rad2deg(action[14])
-# reachy.r_arm.gripper.set_opening(min(100, max(0, action[15] / 2.26 * 100)))
 
 
 reachy.l_arm.goto_joints(action[0:7], duration=2.0, degrees=False)
