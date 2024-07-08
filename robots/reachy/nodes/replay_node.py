e4b42949d2744b44ab23f911d6aa852cf19d33a2
Hennzau <dev@enzo-le-van.fr>
2024-07-08 13:52:08 +0200

    fix import dora-lerobot

--- robots/reachy/nodes/replay_node.py
+++ robots/reachy/nodes/replay_node.py
@@ -1,4 +1,4 @@
-from dora_lerobot.common.datasets.lerobot_dataset import LeRobotDataset
+from lerobot.common.datasets.lerobot_dataset import LeRobotDataset
 import time
 from dora import Node
 import pyarrow as pa
