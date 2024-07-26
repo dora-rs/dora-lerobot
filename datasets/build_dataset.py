1dde54ea01243a491bff2a677343a23bfe9157b9
Hennzau <dev@enzo-le-van.fr>
2024-07-26 17:45:06 +0200

    build_dataset uses Huggingface datasets and hub

--- datasets/build_dataset.py
+++ datasets/build_dataset.py
@@ -1,23 +1,173 @@
 import os
 import argparse
+import warnings
+from dataclasses import dataclass, field
+from typing import ClassVar, Any
 
 import pandas as pd
 import pyarrow as pa
 
+from datasets import Dataset, Features, Image, Sequence, Value
+from datasets.features.features import register_feature
 
-def adapt_timestamps(dataset):
-    start_timestamp = dataset.groupby("episode_index")["timestamp_utc"].min()
+
+def get_episode_info(path):
+    episode_index = pd.read_parquet(path + "/episode_index.parquet")
+    episode_index = episode_index[["timestamp_utc", "episode_index"]]
+    episode_index = pd.DataFrame(episode_index)
+
+    episode_info = []
+
+    for i in range(len(episode_index)):
+        if episode_index.iloc[i]["episode_index"][0] != -1:
+            episode_info.append(
+                (
+                    episode_index.iloc[i]["episode_index"][0],
+                    episode_index.iloc[i]["timestamp_utc"],
+                    None,
+                )
+            )
+        else:
+            episode_info[-1] = (
+                episode_info[-1][0],
+                episode_info[-1][1],
+                episode_index.iloc[i]["timestamp_utc"],
+            )
+
+    if os.path.exists(path + "/failed_episode_index.parquet"):
+        failed_episode_index = pd.read_parquet(path + "/failed_episode_index.parquet")
+        failed_episode_index = failed_episode_index[["failed_episode_index"]]
+
+        failed_episode_index = [
+            i[0] for i in failed_episode_index["failed_episode_index"].tolist()
+        ]
+
+        episode_info = [
+            episode
+            for episode in episode_info
+            if episode[0] not in failed_episode_index
+        ]
+
+    return episode_info
+
+
+def get_observation_images(path):
+    files = os.listdir(path)
+    image_files = [
+        f
+        for f in files
+        if f.startswith("observation.images.") and f.endswith(".parquet")
+    ]
+
+    return [f.replace(".parquet", "") for f in image_files]
+
+
+def adapt_timestamps(dataframe):
+    dataframe["timestamp"] = dataframe["timestamp_utc"]
+
+    start_timestamp = dataframe.groupby("episode_index")["timestamp"].min()
     start_timestamp = start_timestamp.apply(
         lambda x: pd.to_datetime(x).timestamp() * 1000
     )
 
-    dataset["timestamp_utc"] = dataset.apply(
-        lambda x: pd.to_datetime(x["timestamp_utc"]).timestamp() * 1000
+    dataframe["timestamp"] = dataframe.apply(
+        lambda x: pd.to_datetime(x["timestamp"]).timestamp() * 1000
         - start_timestamp[x["episode_index"]],
         axis=1,
     )
 
+    return dataframe
+
+
+def load_dataframe(path, name, episode_info):
+    df = pd.read_parquet(path, f"/{name}.parquet")
+    df = df[["timestamp_utc", "action"]]
+    df = pd.DataFrame(df)
+
+    df["episode_index"] = -1
+    df = [
+        df[
+            (df["timestamp_utc"] >= episode_info[i][1])
+            & (df["timestamp_utc"] <= episode_info[i][2])
+        ]
+        for i in range(len(episode_info))
+    ]
+
+    for i in range(len(df)):
+        df[i].loc[:, "episode_index"] = episode_info[i][0]
+
+    df = pd.concat(df, sort=False)
+    df = adapt_timestamps(df)
+
+    return df
+
+
+def create_dataset(action, state, images):
+    dataset = pd.merge(action, state, on=["timestamp", "episode_index"], how="outer")
+    for i in range(len(images)):
+        dataset = pd.merge(
+            dataset, images[i], on=["timestamp", "episode_index"], how="outer"
+        )
+
+    dataset.sort_values(by=["episode_index", "timestamp"], inplace=True)
+
     return dataset
+
+
+def save_dataset_state(dataset, dataset_name, name):
+    if not os.path.exists("datasets/" + dataset_name):
+        os.makedirs("datasets/" + dataset_name)
+
+    dataset.to_parquet("datasets/" + dataset_name + f"/{name}.parquet", index=False)
+
+
+def fill_dataset(dataset):
+    dataset.set_index(["timestamp", "episode_index"], inplace=True)
+    dataset = dataset.ffill().bfill()
+    dataset.reset_index(inplace=True)
+
+
+@dataclass
+class VideoFrame:
+    pa_type: ClassVar[Any] = pa.struct({"path": pa.string(), "timestamp": pa.float32()})
+    _type: str = field(default="VideoFrame", init=False, repr=False)
+
+    def __call__(self):
+        return self.pa_type
+
+
+with warnings.catch_warnings():
+    warnings.filterwarnings(
+        "ignore",
+        "'register_feature' is experimental and might be subject to breaking changes in the future.",
+        category=UserWarning,
+    )
+    # to make VideoFrame available in HuggingFace `datasets`
+    register_feature(VideoFrame, "VideoFrame")
+
+
+def to_hf_dataset(dataset) -> Dataset:
+    features = {}
+
+    keys = [key for key in dataset if "observation.images." in key]
+    for key in keys:
+        features[key] = VideoFrame()
+
+    features["observation.state"] = Sequence(
+        length=dataset["observation.state"].shape[1],
+        feature=Value(dtype="float32", id=None),
+    )
+    features["action"] = Sequence(
+        length=dataset["action"].shape[1], feature=Value(dtype="float32", id=None)
+    )
+    features["episode_index"] = Value(dtype="int64", id=None)
+    features["frame_index"] = Value(dtype="int64", id=None)
+    features["timestamp"] = Value(dtype="float32", id=None)
+    features["next.done"] = Value(dtype="bool", id=None)
+    features["index"] = Value(dtype="int64", id=None)
+
+    hf_dataset = Dataset.from_dict(dataset, features=Features(features))
+    return hf_dataset
 
 
 def main():
@@ -58,202 +208,66 @@ def main():
     args.dataset_name = args.dataset_name.replace(" ", "_")
     args.dataset_name = args.dataset_name.lower()
 
-    # Load the recording, only keep 'timestamp_utc', and valeus for 'action', 'state', and 'images
-    action = pd.read_parquet(args.record_path + "/action.parquet")
-    action = action[["timestamp_utc", "action"]]
+    episode_info = get_episode_info(args.record_path)
+    images = get_observation_images(args.record_path)
 
-    episode_index = pd.read_parquet(args.record_path + "/episode_index.parquet")
-    episode_index = episode_index[["timestamp_utc", "episode_index"]]
+    action = load_dataframe(args.record_path, "action", episode_info)
+    state = load_dataframe(args.record_path, "observation.state", episode_info)
+    images = [load_dataframe(args.record_path, image, episode_info) for image in images]
 
-    state = pd.read_parquet(args.record_path + "/observation.state.parquet")
-    state = state[["timestamp_utc", "observation.state"]]
+    dataset = create_dataset(action, state, images)
 
-    files = os.listdir(args.record_path)
-    image_files = [
-        f
-        for f in files
-        if f.startswith("observation.images.") and f.endswith(".parquet")
-    ]
-    image_files = [f.replace(".parquet", "") for f in image_files]
+    save_dataset_state(dataset, args.dataset_name, "raw")
 
-    images = [
-        pd.read_parquet(args.record_path + "/" + f + ".parquet") for f in image_files
-    ]
-    images = [images[i][["timestamp_utc", image_files[i]]] for i in range(len(images))]
+    fill_dataset(dataset)
 
-    # filter rows that are between start and end timestamp, and add the episode_index
-    action = pd.DataFrame(action)
-    episode_index = pd.DataFrame(episode_index)
-    state = pd.DataFrame(state)
-    images = [pd.DataFrame(image) for image in images]
+    dataset["frame_index"] = dataset["timestamp"] // int(1000 / framerate)
 
-    # add "episode_index" column to dataframes
-    action["episode_index"] = -1
-    state["episode_index"] = -1
-    for image in images:
-        image["episode_index"] = -1
+    dataset["index"] = dataset["timestamp_utc"] // int(1000 / framerate)
+    dataset.drop(columns=["timestamp_utc"], inplace=True)
 
-    # get a list of tuple (episode, start, end) for each episode: an episode start if episode_index != -1 and end if
-    # episode_index == -1
-    episode_start_end = []
-
-    for i in range(len(episode_index)):
-        if episode_index.iloc[i]["episode_index"] != -1:
-            episode_start_end.append(
-                (
-                    episode_index.iloc[i]["episode_index"],
-                    episode_index.iloc[i]["timestamp_utc"],
-                    None,
-                )
-            )
-        else:
-            episode_start_end[-1] = (
-                episode_start_end[-1][0],
-                episode_start_end[-1][1],
-                episode_index.iloc[i]["timestamp_utc"],
-            )
-
-    # for each dataframe, filter rows that are between start and end timestamp, and add the episode_index
-    action = [
-        action[
-            (action["timestamp_utc"] >= episode_start_end[i][1])
-            & (action["timestamp_utc"] <= episode_start_end[i][2])
-        ]
-        for i in range(len(episode_start_end))
-    ]
-
-    for i in range(len(action)):
-        action[i].loc[:, "episode_index"] = episode_start_end[i][0][0]
-
-    action = pd.concat(action, sort=False)
-
-    state = [
-        state[
-            (state["timestamp_utc"] >= episode_start_end[i][1])
-            & (state["timestamp_utc"] <= episode_start_end[i][2])
-        ]
-        for i in range(len(episode_start_end))
-    ]
-
-    for i in range(len(state)):
-        state[i].loc[:, "episode_index"] = episode_start_end[i][0][0]
-
-    state = pd.concat(state, sort=False)
-
-    for i in range(len(images)):
-        image = [
-            images[i][
-                (images[i]["timestamp_utc"] >= episode_start_end[j][1])
-                & (images[i]["timestamp_utc"] <= episode_start_end[j][2])
-            ]
-            for j in range(len(episode_start_end))
-        ]
-
-        for j in range(len(image)):
-            image[j].loc[:, "episode_index"] = episode_start_end[j][0][0]
-
-        images[i] = pd.concat(image, sort=False)
-
-    state = adapt_timestamps(state)
-    action = adapt_timestamps(action)
-
-    for i in range(len(images)):
-        # multiply the timestamp by the value of the image column
-        images[i]["timestamp_utc"] = images[i].apply(
-            lambda x: x[image_files[i]][0]["timestamp"] * 1000, axis=1
-        )
-
-    # we merge action, state, and images into a single dataframe, based on the timestamp_utc. Preserve the order of
-    # the rows and 'episode_index' column
-
-    dataset = pd.merge(
-        action, state, on=["timestamp_utc", "episode_index"], how="outer"
-    )
-    for i in range(len(images)):
-        dataset = pd.merge(
-            dataset, images[i], on=["timestamp_utc", "episode_index"], how="outer"
-        )
-
-    # load failed_episode_index.parquet
-    if os.path.exists(args.record_path + "/failed_episode_index.parquet"):
-        failed_episode_index = pd.read_parquet(
-            args.record_path + "/failed_episode_index.parquet"
-        )
-        failed_episode_index = failed_episode_index[["failed_episode_index"]]
-
-        failed_episode_index = [
-            i[0] for i in failed_episode_index["failed_episode_index"].tolist()
-        ]
-
-        # remove the rows for which episode_index is in failed_episode_index
-        dataset = dataset[~dataset["episode_index"].isin(failed_episode_index)]
-
-    # sort the dataset by episode_index and timestamp_utc
-
-    dataset.sort_values(by=["episode_index", "timestamp_utc"], inplace=True)
-
-    # create a raw.parquet file with the dataset
-    if not os.path.exists("datasets/" + args.dataset_name):
-        os.makedirs("datasets/" + args.dataset_name)
-
-    dataset.to_parquet("datasets/" + args.dataset_name + "/raw.parquet", index=False)
-
-    # for each row with NaN values, fill the NaN values with the nearest non-NaN value
-    dataset.set_index(["timestamp_utc", "episode_index"], inplace=True)
-
-    dataset = dataset.ffill().bfill()
-
-    dataset.reset_index(inplace=True)
-
-    # add a new column "frame" to the dataset that is the result of timestamp_utc // framerate
-    dataset["frame"] = dataset["timestamp_utc"] // int(1000 / framerate)
-
-    # Only keep 1 row for each frame, keep the first row if there are multiple rows for the same frame
     dataset = (
-        dataset.groupby(["episode_index", "frame"])
+        dataset.groupby(["episode_index", "frame_index"])
         .apply(lambda x: x.iloc[0], include_groups=False)
         .reset_index()
     )
 
-    # Remove timestamp_utc column
-    dataset.drop(columns=["timestamp_utc"], inplace=True)
+    dataset.sort_values(by=["episode_index", "timestamp"], inplace=True)
 
-    # Add new column "timestamp" that is the result of frame * framerate
-    dataset["timestamp"] = dataset["frame"] * 1000 / framerate
+    dataset["next.done"] = False
 
-    # Drop the frame column
-    dataset.drop(columns=["frame"], inplace=True)
+    for episode_index in dataset["episode_index"].unique():
+        last_frame = dataset[dataset["episode_index"] == episode_index].iloc[-1]
+        dataset.loc[
+            (dataset["episode_index"] == episode_index)
+            & (dataset["frame_index"] == last_frame["frame_index"]),
+            "next.done",
+        ] = True
 
-    # only keep the array of positions for each action abd state
-    arrow_struct = pa.StructArray.from_pandas(dataset["action"][0])
-    joints = arrow_struct.field("joints").to_numpy(zero_copy_only=False)
+    joints = pa.StructArray.from_pandas(dataset["action"][0])
+    joints = joints.field("joints").to_numpy(zero_copy_only=False)
+    dataset["joints"] = pd.Series([joints for _ in range(len(dataset))])
 
     def get_values(x):
-        arrow_x = pa.StructArray.from_pandas(x)
-        return arrow_x.field("values").to_numpy(zero_copy_only=False)
+        x = pa.StructArray.from_pandas(x)
+        return x.field("values").to_numpy(zero_copy_only=False)
 
     dataset["action"] = dataset["action"].apply(lambda x: get_values(x))
     dataset["observation.state"] = dataset["observation.state"].apply(
         lambda x: get_values(x)
     )
 
-    # Add a new column "joints" that is the array of joints with enough rows to match the number of rows in the dataset
-    dataset["joints"] = pd.Series([joints for _ in range(len(dataset))])
-
     dataset.sort_values(by=["episode_index", "timestamp"], inplace=True)
+    save_dataset_state(dataset, args.dataset_name, "dataset")
 
-    # save the dataset to a parquet file
-    dataset.to_parquet(
-        "datasets/" + args.dataset_name + "/dataset.parquet", index=False
-    )
-
-    # move the video folder to the dataset folder
     if os.path.exists(args.record_path + "/videos"):
         os.rename(
             args.record_path + "/videos", "datasets/" + args.dataset_name + "/videos"
         )
 
-    print(dataset)
+    hf_dataset = to_hf_dataset(dataset)
+
+    hf_dataset.save_to_disk(f"datasets/{args.dataset_name}/hf_dataset")
 
 
 if __name__ == "__main__":
